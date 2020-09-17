6fc92494b3c9e49a39ffdcebd7897663eaa2bdbe
zddhandsome <1640925375@qq.com>
2020-09-17 21:01:48 +0800

    di

--- src/hellopro/hellopro.java
+++ src/hellopro/hellopro.java
@@ -1,10 +1,10 @@
 package hellopro;
 
-public class test1 {
+public class hellopro {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("HellowWorld");
+		System.out.println("hello world");
 	}
 
 }
